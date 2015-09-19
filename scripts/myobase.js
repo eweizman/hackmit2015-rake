81aeabcbe613389dc02bb9f66c1986282662705a
eweizman <eden_weizman@brown.edu>
2015-09-19 16:26:02 -0400

    myo

--- scripts/myobase.js
+++ scripts/myobase.js
@@ -1,12 +1,18 @@
 function setup() {
 	alert("this was called!");
-	var Myo = require('myo');
-	alert("loading worked");
+	//var Myo = Myo;
+	//alert("loading worked");
 
 	//Start talking with Myo Connect
 	Myo.connect();
+	Myo.on('connected', function() {
+		debugger
+		console.log("connected successfully!!");
+		this.vibrate();
+	});
 
-	Myo.on('fist', function(){
+	Myo.on('pose', function(myo, data){
+		debugger
     	console.log('Hello Myo!');
     	this.vibrate();
 	});
